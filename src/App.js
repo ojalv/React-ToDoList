28a6a1b438ea7c30c5a18ec6491cb7cfc59b847d
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-08-31 15:07:28 -0300

    Componentes de TODO Machine

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList.js';
+import { TodoItem } from './TodoItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
 
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      </TodoList>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
+      {/* <CreateTodoButton/> */}
     </div>
   );
 }
 
-function TodoItem() {
-  return (
-    <li>
-      <span>V</span>
-      <p>Ganar mucho dinero</p>
-      <span>X</span>
-    </li>
-  )
-}
+
+
 
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+      <li>
+        <span>V</span>
+        <p>Ganar dinero</p>
+        <span>X</span>
+      </li>
+    )
+  }
+
+export {TodoItem}
