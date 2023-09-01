924b34247ec1b7664c7713b734396d80da642317
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-01 14:19:04 -0300

    Estilos CSS en React: Agregar estilos en linea; Agregar estilos desde archivo.css; Equivalencia entre React.Fragment y <>.

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,13 @@
 
+import './TodoCounter.css';
 function TodoCounter({ total, completed }) {
     return (
-      <h1>
+      <h1 style={{
+        fontSize: '24px',
+        textAlign: 'center',
+        margin: '0px',
+        padding:'48px'
+      }}> 
         Has completado {completed} de {total} TODOS
       </h1>
     );
