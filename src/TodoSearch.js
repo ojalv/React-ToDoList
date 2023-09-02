4699a18477b800c7ed081f44bc969b70242d411d
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-01 22:31:11 -0300

    Transmision de estados desde componentes padres hacia componentes hijos; Estados derivados
    Un estado derivado en React es un estado que se calcula o deriva a partir de otros estados o propiedades en un componente. En lugar de almacenarse directamente, se calcula en función de los datos existentes y se actualiza automáticamente cuando esos datos cambian. Esto permite mantener el estado del componente de manera eficiente y asegura que siempre esté sincronizado con los datos subyacentes. Los estados derivados se implementan generalmente utilizando Hooks como `useState`, `useEffect`, `useMemo` o `useCallback`, y son útiles para realizar cálculos, transformaciones o agregaciones basados en los datos actuales de un componente React.

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({
+  searchValue,
+  setSearchValue
+}) {
 
-  const [searchValue, setSearchValue] = React.useState('');
-  console.log('Los usuarios buscan todos de ' + searchValue);
-  
+
   return (
     <input
       placeholder="Cortar cebolla"
