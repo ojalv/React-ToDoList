5173d99899354764792a98a1ba825f6d3a50fac4
Alvaro Ojeda <135424514+ojalv@users.noreply.github.com>
2023-09-03 16:41:42 -0300

    Iconos en React: librerías y SVG

---
+++ src/DeleteIcon.js
@@ -0,0 +1,5 @@
+import React from 'react';import{TodoIcon}from './TodoIcon';function DeleteIcon({onDelete}){return(<TodoIcon
+  type="delete"
+  color="gray"
+  onClick={onDelete}/>);}
+  export{DeleteIcon};
